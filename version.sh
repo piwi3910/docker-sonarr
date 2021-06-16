6cae5a5c84b1640d54208a77ab54676816c8ae9b
Pascal Watteel <pascal@watteel.be>
2021-06-16 13:47:44 +0400

    add jq to install pre-req

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
-echo "Finding latest Sabnzbd release and set it as a env.variable to use during docker build"
+echo "Finding latest Sonarr release and set it as a env.variable to use during docker build"
 apt update -y
-apt install curl grep -y
+apt install curl grep jq -y
 version=$(curl --silent "https://api.github.com/repos/Sonarr/Sonarr/tags" | jq -r '.[0].name')
 echo "version=$version" >> vars.env
 
