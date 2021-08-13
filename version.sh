b40a38e3302778d670811e3e54efbb42c0ece7d9
Pascal Watteel <pascal@watteel.be>
2021-08-13 11:26:11 +0400

    cleanup

--- version.sh
+++
@@ -1,6 +0,0 @@
-echo "Finding latest Sonarr release and set it as a env.variable to use during docker build"
-apt update -y
-apt install curl grep jq sed -y
-version=$(curl --silent "https://api.github.com/repos/Sonarr/Sonarr/tags" | jq -r '.[0].name' | sed -r 's/^.{1}//')
-echo "version=$version" >> vars.env
-
